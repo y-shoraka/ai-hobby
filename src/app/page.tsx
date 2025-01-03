e3d5235a402619a755ab7b709bc7669ec8d0bf46
y.shoraka <shorakayasaman@gmail.com>
2025-01-03 17:05:00 +0330

    feat:install shadcn package with bun

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  console.log("heelloooo");
-  return <div>hello guys...!</div>;
+  return (
+    <div>
+      hello guys...!
+      <Button>testing button</Button>
+    </div>
+  );
 }
