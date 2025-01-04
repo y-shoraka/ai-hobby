4bd647fd9d96342045dd97baf5e3960e41715428
y.shoraka <shorakayasaman@gmail.com>
2025-01-04 21:01:55 +0330

    fix: fix the build errors

--- src/common/components/providers/providers.tsx
+++ src/common/components/providers/providers.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React, { ReactNode } from "react";
+import React from "react";
 
 const queryClient = new QueryClient();
 
